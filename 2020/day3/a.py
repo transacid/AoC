01e36977761b418bff84ed0d9ff641e5e19718b9
Boris Petersen <transacid@centerim.org>
2020-12-06 18:57:44 +0100

    refactor day 3 a in light of b

--- 2020/day3/a.py
+++ 2020/day3/a.py
@@ -3,22 +3,25 @@ def get_input():
         return [line.strip() for line in f.readlines()]
 
 
-def run(field):
+def run(field, r, d):
     x = 0
     y = 0
     line = len(field[0])
     trees = 0
     len_map = len(field)
+    len_field = len(field)
     while len_map > 0:
+        if x > len_field:
+            break
         if y >= line:
             y = y - line
         pos = field[x][y]
         len_map -= 1
         if pos == "#":
             trees += 1
-        x, y = x + 1, y + 3
-    print(trees)
+        x, y = x + d, y + r
+    return trees
 
 
 if __name__ == '__main__':
-    run(get_input())
+    print(run(get_input(), 3, 1))
