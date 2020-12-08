f7cca0fcaf2b87cb0d679745e05b470e88af7752
Boris Petersen <transacid@centerim.org>
2020-12-08 20:17:23 +0100

    day 5 b

---
+++ 2020/day5/b.py
@@ -0,0 +1,35 @@
+def get_input():
+    with open("input.txt", "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def run(passes):
+    rows = [r for r in range(128)]
+    cols = [r for r in range(8)]
+    for action in passes:
+        rlength = len(rows)
+        clength = len(cols)
+        if action == "F":
+            del rows[int(rlength / 2):]
+        elif action == "B":
+            del rows[:int(rlength / 2)]
+        elif action == "R":
+            del cols[:int(clength / 2)]
+        elif action == "L":
+            del cols[int(clength / 2):]
+    final_row = rows[0]
+    final_col = cols[0]
+    return final_row * 8 + final_col
+
+
+if __name__ == '__main__':
+    bpasses = []
+    for boardinpass in get_input():
+        bpass = run(boardinpass)
+        bpasses.append(bpass)
+    so = sorted(bpasses)
+    it = None
+    for i in so:
+        if i % 2 == it:
+            print(i - 1)
+        it = i % 2
