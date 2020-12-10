c87b65f7a89d8cdb28598fd8c9f823adddddef7a
Boris Petersen <transacid@centerim.org>
2020-12-10 22:14:31 +0100

    day 6 a

---
+++ 2020/day6/a.py
@@ -0,0 +1,25 @@
+import re
+import string
+
+
+def get_input():
+    with open("input.txt", "r") as f:
+        batch = f.read()
+        sl = re.split('\n\n', batch)
+        dict_list = []
+        for passport in sl:
+            passports = re.sub(r"\n", " ", passport).strip().split(" ")
+            dict_list.append(passports)
+        return dict_list
+
+
+def run(passes):
+    yeses = 0
+    for group in passes:
+        indi_yes = len("".join(set("".join(group))))
+        yeses = yeses + indi_yes
+    return yeses
+
+
+if __name__ == '__main__':
+    print(run(get_input()))
