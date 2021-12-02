e91ecdaad202abdb99dd20122dca918c575295e6
Boris Petersen <transacid@gmail.com>
2021-12-02 10:27:55 +0100

    day 2 part b

---
+++ 2021/day2/b.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type command struct {
+	cmd    string
+	amount int
+}
+
+func rf() []command {
+	f, err := os.ReadFile("input.txt")
+	check(err)
+	s := bytes.Split(bytes.Trim(f, "\n\n"), []byte("\n"))
+	var c []command
+	for _, l := range s {
+		b := bytes.Split(l, []byte(" "))
+		com := string(b[0])
+		cou, _ := strconv.Atoi(string(b[1]))
+		c = append(c, command{com, cou})
+	}
+	return c
+}
+
+func main() {
+	m := rf()
+	var hpos, vpos, aim int
+	for _, cmd := range m {
+		if cmd.cmd == "forward" {
+			hpos += cmd.amount
+			vpos += cmd.amount * aim
+		} else if cmd.cmd == "down" {
+			aim += cmd.amount
+		} else if cmd.cmd == "up" {
+			aim -= cmd.amount
+		}
+	}
+	fmt.Println(hpos * vpos)
+}
