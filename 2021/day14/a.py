5d9d740c9c778b6c48fb951e9705a376cb881df4
Boris Petersen <transacid@gmail.com>
2021-12-14 15:57:36 +0100

    day 14 part a

---
+++ 2021/day14/a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+
+def get_input():
+    with open("input.txt", "r") as f:
+        template = f.readline().strip()
+        f.readline()
+        return template, f.read().strip()
+
+
+if __name__ == '__main__':
+    tl, inputs = get_input()
+    d = dict(x.split(" -> ") for x in inputs.split("\n"))
+    for i in range(10):
+        l = []
+        for i, c in enumerate(tl):
+            l.append(tl[i])
+            try:
+                l.append(d[tl[i:i+2]])
+            except KeyError:
+                break
+        tl = "".join(l)
+    c = Counter(tl).most_common()
+    max = c[0][1]
+    min = c[-1][1]
+    print(max - min)
