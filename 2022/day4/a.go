5d1bfab0890200d239af4dc93e0cb07592a243ac
Boris Petersen <bp@boris-petersen.de>
2022-12-04 13:02:19 +0100

    2022 day 4

---
+++ 2022/day4/a.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	c := gfi()
+	a := strings.Split(strings.TrimRight(string(c), "\n"), "\n")
+	var res int
+	for _, p := range a {
+		pairs := strings.Split(p, ",")
+		pair1 := strings.Split(pairs[0], "-")
+		pair2 := strings.Split(pairs[1], "-")
+		p11, _ := strconv.Atoi(pair1[0])
+		p12, _ := strconv.Atoi(pair1[1])
+		p21, _ := strconv.Atoi(pair2[0])
+		p22, _ := strconv.Atoi(pair2[1])
+		if (p11 >= p21 && p12 <= p22) || (p21 >= p11 && p22 <= p12) {
+			res++
+		}
+	}
+	fmt.Printf("%v\n", res)
+}
+
+func gfi() []byte {
+	f, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
