7b666afb90d703eadb6988e03325727455787b3d
Boris Petersen <bp@boris-petersen.de>
2022-12-08 21:47:03 +0100

    2022 day 5

---
+++ 2022/day5/b.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	c := gfi()
+	a := strings.Split(string(c), "\n")
+	stacks := make(map[int][]string)
+	var ins [][]string
+	for _, line := range a {
+		if strings.HasPrefix(line, " 1") || line == "" {
+			continue
+		} else if strings.HasPrefix(line, "move") {
+			ins = append(ins, buildinstruction(line))
+		} else {
+			buildstack(line, stacks)
+		}
+	}
+	for _, stack := range stacks {
+		revSlice(stack)
+	}
+	for _, instruction := range ins {
+		execute(instruction, stacks)
+	}
+	var res []string
+	for i := 1; i <= len(stacks); i++ {
+		res = append(res, stacks[i][len(stacks[i])-1])
+	}
+	fmt.Printf("%s\n", strings.Join(res, ""))
+}
+
+func gfi() []byte {
+	f, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func buildstack(line string, stacks map[int][]string) map[int][]string {
+	sl := strings.Split(line, "")
+	counter := 1
+	for i, c := range sl {
+		if i%4 == 1 {
+			if c != " " {
+				stacks[counter] = append(stacks[counter], c)
+			}
+			counter++
+		}
+	}
+	return stacks
+}
+
+func buildinstruction(line string) []string {
+	re := regexp.MustCompile(`move (\d+) from (\d+) to (\d+)`)
+	ins := re.FindStringSubmatch(line)
+	return ins[1:]
+}
+
+func execute(ins []string, stacks map[int][]string) {
+	times := sureAtoi(ins[0])
+	from := sureAtoi(ins[1])
+	to := sureAtoi(ins[2])
+
+	move := stacks[from][len(stacks[from])-times:]
+	stacks[from] = stacks[from][:len(stacks[from])-times]
+	for _, s := range move {
+		stacks[to] = append(stacks[to], s)
+	}
+}
+
+func revSlice(slice []string) []string {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+	return slice
+}
+
+func sureAtoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
